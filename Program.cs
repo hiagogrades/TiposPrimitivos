cd649001dd7842ec9550dc075ebc182d0a14b63e
Hiago Grades <hiagogrades@hotmail.com>
2022-06-15 17:08:40 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TiposPrimitivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
